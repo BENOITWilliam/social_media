1fe1cb62c277164ab2f5ace3474b55dd744323e6
blezzou <113239335+blezzou@users.noreply.github.com>
2024-05-28 15:34:53 +0200

    Add files via upload

--- web/traitement_crea.php
+++ web/traitement_crea.php
@@ -1,6 +1,7 @@
 <?php
 
 $database = "likedin";
+$erreur = false;
 session_start();
 
 try {
@@ -31,54 +32,58 @@ if ($db_found) {
     $pseudo = isset($_POST['pseudo']) ? $_POST['pseudo'] :'';
     $mdp = isset($_POST['mdp']) ? $_POST['mdp'] :'';
 
-    if(empty($mail) || empty($pseudo) || empty($mdp)){
-        echo '<div class="error-container">';
-        if(empty($mail)){echo '<p class="error-message">Veuillez saisir une adresse mail</p></br>';}
-        if(empty($pseudo)){echo '<p class="error-message">Veuillez saisir un pseudo</p></br>';}
-        if(empty($mdp)){echo '<p class="error-message">Veuillez saisir un mot de passe</p></br>';}
-        
+    echo '<div class="error-container">';
 
-        echo '</br><a href=crea.php><button class="btn btn-dark">Page De Création De Compte</button></a>';
+    if(empty($mail) || empty($pseudo) || empty($mdp)){
+        $erreur = true;
+        if(empty($mail)){echo '<p class="error-message">Veuillez saisir une adresse mail</p>';}
+        if(empty($pseudo)){echo '<p class="error-message">Veuillez saisir un pseudo</p>';}
+        if(empty($mdp)){echo '<p class="error-message">Veuillez saisir un mot de passe</p>';}
     }
 
     // check si le pseudo existe déjà
     $sql = "SELECT * FROM `utilisateur` WHERE Pseudo LIKE '".$pseudo."'";
     $result = mysqli_query($db_handle, $sql);
-    $res = mysqli_num_rows($result);
-    if ( ($res) > 0) {
-        echo '<div class="error-container">';
-        echo '<p class="error-message">Ce pseudo existe déjà</p></br>';
-        echo '</br><a href=crea.php><button class="btn btn-dark">Page De Création De Compte</button></a>';
+    $res1 = mysqli_num_rows($result);
+    if ( ($res1) > 0) {
+        $erreur = true;
+        echo '<p class="error-message">Ce pseudo existe déjà</p>';
     }
 
     // check si le mail existe déjà
     $sql = "SELECT * FROM `utilisateur` WHERE Email Like '".$mail."'";
     $result = mysqli_query($db_handle, $sql);
-    $res = mysqli_num_rows($result);
-    if ( ($res) > 0) {
-        echo '<div class="error-container">';
-        echo '<p class="error-message">Ce mail est déjà utilisé</p></br>';
+    $res2 = mysqli_num_rows($result);
+    if ( ($res2) > 0) {
+        $erreur = true;
+        echo '<p class="error-message">Ce mail est déjà utilisé</p>';
+    }
+
+    if (($erreur) == true) {
         echo '</br><a href=crea.php><button class="btn btn-dark">Page De Création De Compte</button></a>';
     }
+    
 
     /*$sql = "SELECT MAX(id) AS max_id FROM utilisateur";
     $resultid = mysqli_query($db_handle, $sql);
     $new_id = $resultid + 1;*/
 
     // Insert new user into the database with the new id
-    $sql = "INSERT INTO `utilisateur` (Pseudo, Email, MDP,NC) VALUES ('".$pseudo."','".$mail."','".$mdp."','0')";
+    if (!empty($mail) && !empty($pseudo) && !empty($mdp) && ($res1) == 0 && ($res2) == 0){
+        $sql = "INSERT INTO `utilisateur` (Pseudo, Email, MDP,NC) VALUES ('".$pseudo."','".$mail."','".$mdp."','0')";
 
-    try{
-        $result = mysqli_query($db_handle, $sql);
-      }
-      catch (Exception $e){
-        $error = $e->getMessage();
-        echo $error;
-        exit();
-    }
+        try{
+            $result = mysqli_query($db_handle, $sql);
+          }
+          catch (Exception $e){
+            $error = $e->getMessage();
+            echo $error;
+            exit();
+        }    
     echo '<div class="bienvenue-container">';
     echo '<p class="bienvenue-message">Bienvenue chez nous!</p></br>';
     echo '</br><a href=index.html><button class="btn btn-dark">Continuer en vous connectant</button></a>';
+    }
 
 }
 ?>
