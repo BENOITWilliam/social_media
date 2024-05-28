e24a1f983ab9aef48348499135d5c366c14af7e1
Sowgan <131462087+BENOITWilliam@users.noreply.github.com>
2024-05-28 17:31:43 +0200

    Update offre_emploi.php

--- web/offre_emploi.php
+++ web/offre_emploi.php
@@ -52,52 +52,71 @@ echo '<div class="container" id="color"><div class="center_nav"><nav class="navb
 
 if ($db_found) {
 
-    echo '<br><br><br><div class="container" id="color"><h1>Recherche travail :</h1><br>
-    <form method="POST">
-    <input type="text" name="recherche" placeholder="recherche" class="form-control"/><br><br>
-    <button type="submit" name="soumettre" value="soumettre" class="btn btn-primary">Rechercher</button></br>
-    </form>
-    <br>
-    </div>';
+  echo '<br><br><br><div class="container" id="color"><h1>Recherche travail :</h1><br>
+  <form method="POST">
+  <input type="text" name="recherche" placeholder="recherche" class="form-control"/><br><br>
+  <button type="submit" name="soumettre" value="soumettre" class="btn btn-primary">Rechercher</button></br>
+  </form>
+  <br>
+  </div>';
 
-    $sql = "SELECT *  FROM `emploi`";
+  $sql = "SELECT *  FROM `emploi`";
 
-    if(array_key_exists('soumettre',$_POST)){
-        $recherche = isset($_POST['recherche']) ? $_POST['recherche'] :'';
-    
-        $sql="SELECT * FROM `emploi` WHERE Nom LIKE '%$recherche%'";
-    }
+  if(array_key_exists('soumettre',$_POST)){
+      $recherche = isset($_POST['recherche']) ? $_POST['recherche'] :'';
+  
+      $sql="SELECT * FROM `emploi` WHERE Nom LIKE '%$recherche%'";
+  }
 
-    try{
-        $result = mysqli_query($db_handle, $sql);
-    }
-    catch (Exception $e){
-        $error = $e->getMessage();
-        echo $error;
-        exit();
-    }
+  try{
+      $result = mysqli_query($db_handle, $sql);
+  }
+  catch (Exception $e){
+      $error = $e->getMessage();
+      echo $error;
+      exit();
+  }
 
-    echo '<br><br><br><div class="container-sm" id="color">';
+  echo '<br><br><br><div class="container-sm" id="color">';
 
-    echo "<table class='table'> <thead class='thead-dark'> <tr> <th scope='col'>Nom</th> <th scope='col'>Description</th> <th scope='col'>Image</th> 
-    <th scope='col'>Regarder</th> <tbody>";
+  echo "<table class='table'> <thead class='thead-dark'> <tr> <th scope='col'>Nom</th> <th scope='col'>Description</th> <th scope='col'>Image</th> 
+  <th scope='col'>Regarder</th> <tbody>";
 
-    while ($data = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $data['Nom'] . "</td>";
-        echo "<td> " . $data['Desc_courte'] . "</td>";
-        echo '<td><img src="'.$data['Image'].'" class="img-thumbnail" width="100px" height="100px"></td>';
-        echo '<td><form method="post">
-        <input type="submit" name="reg_'.$data['ID_Emploi'].'" class="btn btn-success" value="Regarder"></input>
-        </form></td>';
-        echo "</tr>";
-    
-        if(array_key_exists('reg_'.$data['ID_Emploi'].'',$_POST)){
-          $_SESSION['ID_Emploi'] = $data['ID_Emploi'];
-          echo '<meta http-equiv="refresh" content="0; url=http://localhost/web/regarder_emploi.php">';
-        }
-    }
-    echo "</tbody> </table></div>";
+  while ($data = mysqli_fetch_assoc($result)) {
+      echo "<tr>";
+      echo "<td>" . $data['Nom'] . "</td>";
+      echo "<td> " . $data['Desc_courte'] . "</td>";
+      echo '<td><img src="'.$data['Image'].'" class="img-thumbnail" width="100px" height="100px"></td>';
+      echo '<td><form method="post">
+      <input type="submit" name="reg_'.$data['ID_Emploi'].'" class="btn btn-success" value="Regarder"></input>
+      </form></td>';
+      echo "</tr>";
+  
+      if(array_key_exists('reg_'.$data['ID_Emploi'].'',$_POST)){
+        $_SESSION['ID_Emploi'] = $data['ID_Emploi'];
+        echo '<meta http-equiv="refresh" content="0; url=http://localhost/web/regarder_emploi.php">';
+      }
+  }
+  echo "</tbody> </table></div>";
+
+  echo '</div><div class="container"><br><br><br><br><br><br></div></body>';
+  echo '<footer>
+  <div class="container" id="colorb"><br>
+      <div class="row">
+      <div class="col-sm-4">
+          <center><p id="txt_color">Copyright © 2024 Volpe Inc. Tous droits réservés.</p></center>
+          <center><p id="txt_color">France</p></center>
+      </div>
+      <div class="col-sm-4">
+          <center><a href="#"><p id="txt_color">Politique de confidentialité</p></a></center>
+          <center><a href="#"><p id="txt_color">Politique relative aux cookies</p></a></center>
+      </div>
+      <div class="col-sm-4">
+          <center><a href="#"><p id="txt_color">Politique</p></a></center>
+          <center><a href="#"><p id="txt_color">Conditions générales d\'utilisation</p></a></center>
+      </div>
+  </div>
+  </footer>';
 
 }
 
