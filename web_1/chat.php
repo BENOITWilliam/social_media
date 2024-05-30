7d5872fae511bc7480cd631125c5698a03585a2d
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2024-05-30 12:31:29 +0200

    création de la partie graphique html

--- web_1/chat.php
+++ web_1/chat.php
@@ -71,3 +71,12 @@ else
         }  
     }
 } ?>
+<!DOCTYPE html>
+    <html>
+        <head> 
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        </head>
+        <body>
+            <div class="container w-50" id="color">
+                <div >
