e0a2e32d0519ce596c161045f116d61ec17961c9
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2024-05-31 12:31:19 +0200

    init fonction  connexion base

--- web_1/chargement_messages.php
+++ web_1/chargement_messages.php
@@ -2,3 +2,21 @@
 $database = "likedin";
 session_start();
 $iddetinataire = $_GET['ID'];
+
+try {
+    $db_handle = mysqli_connect('localhost', 'root', 'root');
+}
+catch (Exception $e){
+    $error = $e->getMessage();
+    echo $error;
+    exit();
+}
+
+try{
+    $db_found = mysqli_select_db($db_handle, $database);
+}
+catch (Exception $e){
+    $error = $e->getMessage();
+    echo $error;
+    exit();
+}
