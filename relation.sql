482e7d823f89cf834fe71cc3d8b9b2558913276f
blezzou <113239335+blezzou@users.noreply.github.com>
2024-05-31 16:35:34 +0200

    Add files via upload

---
+++ relation.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3308
+-- Généré le : ven. 31 mai 2024 à 14:32
+-- Version du serveur : 8.2.0
+-- Version de PHP : 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `likedin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `relation`
+--
+
+DROP TABLE IF EXISTS `relation`;
+CREATE TABLE IF NOT EXISTS `relation` (
+  `ID_relation` int NOT NULL AUTO_INCREMENT,
+  `ID_demandeur_ami` int DEFAULT NULL,
+  `ID_ami` int DEFAULT NULL,
+  PRIMARY KEY (`ID_relation`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
+
+--
+-- Déchargement des données de la table `relation`
+--
+
+INSERT INTO `relation` (`ID_relation`, `ID_demandeur_ami`, `ID_ami`) VALUES
+(1, 2, 1),
+(2, 1, 2),
+(3, 18, 1),
+(4, 18, 2);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
