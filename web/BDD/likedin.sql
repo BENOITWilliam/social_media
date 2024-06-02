64d008c0d761964224c0901e47b810f3134b22e9
paulgandelin <131462237+paulgandelin@users.noreply.github.com>
2024-06-02 19:06:03 +0200

    Update likedin.sql

--- web/BDD/likedin.sql
+++ web/BDD/likedin.sql
@@ -2,10 +2,10 @@
 -- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:3306
--- Généré le : sam. 01 juin 2024 à 17:19
--- Version du serveur : 5.7.24
--- Version de PHP : 8.3.1
+-- Host: localhost:3306
+-- Generation Time: Jun 02, 2024 at 05:05 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `likedin`
+-- Database: `likedin`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `emploi`
+-- Table structure for table `emploi`
 --
 
 CREATE TABLE `emploi` (
@@ -37,17 +37,18 @@ CREATE TABLE `emploi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `emploi`
+-- Dumping data for table `emploi`
 --
 
 INSERT INTO `emploi` (`ID_Emploi`, `Nom`, `Desc_courte`, `Description`, `Image`, `Date`) VALUES
-(1, 'Jgl teemo', 'simple teemo flash', 'un ptit demon', 'documents/emploi/teemo.png', NULL),
-(3, 'Pilote F1', 'Cherche Pilote de F1', 'Cherche un pilote de F1 pour remplacer Ocon. Ce pilote doit avoir un talent et des sponsors qui le suivent. On cherche quelqu\'un qui suis les consignes d\'équipes et qui ne dive pas par folie son coéquipier car on aimerait gagner des points.', 'documents/emploi/alpine.png', NULL);
+(1, 'Jgl teemo', 'simple teemo flash', 'un ptit demon', 'documents/emploi/teemo.png', '2024-05-30'),
+(3, 'Pilote F1', 'Cherche Pilote de F1', 'Cherche un pilote de F1 pour remplacer Ocon. Ce pilote doit avoir un talent et des sponsors qui le suivent. On cherche quelqu\'un qui suis les consignes d\'équipes et qui ne dive pas par folie son coéquipier car on aimerait gagner des points.', 'documents/emploi/alpine.png', '2024-05-30'),
+(8, 'ingénieur', 'efé', 'éea', 'documents/site/emploi.jpg', '2024-06-01');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `event`
+-- Table structure for table `event`
 --
 
 CREATE TABLE `event` (
@@ -62,17 +63,115 @@ CREATE TABLE `event` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `event`
+-- Dumping data for table `event`
 --
 
 INSERT INTO `event` (`ID_Event`, `ID_Emetteur`, `Lien`, `Nom`, `Description`, `Date`, `Heure`, `Lieu`) VALUES
 (3, 1, 'documents/event/17018.jpg', 'test', '', '2024-05-31', '', ''),
-(4, 5, 'documents/event/event.jpg', 'TestFinal', 'TestFinal', '2024-06-02', '', '');
+(4, 5, 'documents/event/event.jpg', 'TestFinal', 'TestFinal', '2024-06-02', '', ''),
+(5, 2, 'documents/event/Capture d\'écran 2023-03-08 012515.png', 'RDV CINE', 'On va voir le dernier James Bond', '2024-06-13', '03:41', 'Cinéma Beaugrenelle');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `post`
+-- Table structure for table `message`
+--
+
+CREATE TABLE `message` (
+  `id_message` int(11) NOT NULL,
+  `Message` text,
+  `id_Destinataire` int(11) DEFAULT NULL,
+  `id_Auteur` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `message`
+--
+
+INSERT INTO `message` (`id_message`, `Message`, `id_Destinataire`, `id_Auteur`) VALUES
+(21, 'Salut tu joues', 1, 2),
+(22, '?', 1, 2),
+(23, 'oui et toi', 2, 1),
+(24, 'perso teemo jungle', 2, 1),
+(25, 'et kayle supp', 2, 1),
+(26, 'pourquoi pas un yummi top ?', 2, 1),
+(47, 'En vrai', 1, 2),
+(52, 'flash q et frist blood ', 1, 2),
+(53, 'salut', 1, 3),
+(54, 'noob', 1, 2),
+(55, 'coucou', 1, 2),
+(56, 'comment tu vas\r\n', 1, 3),
+(57, 'tu fais quoi ?', 2, 1),
+(58, 'merci pour le canon', 2, 1),
+(59, 'tu touches ?\r\n', 1, 2),
+(60, 're', 1, 2),
+(61, 're', 1, 2),
+(62, 're', 2, 1),
+(63, 'cq', 1, 2),
+(64, 'coucou', 2, 1),
+(65, 'salut teemo !!!', 1, 2),
+(66, 'Coucou', 2, 1),
+(67, 'slt', 2, 1),
+(68, 'slt', 2, 1),
+(69, 'ds', 1, 2),
+(70, 'slt', 2, 1),
+(71, 'de\r\n', 1, 2),
+(72, 'fr', 1, 2),
+(73, 'la putain de sa mère\r\n', 2, 1),
+(74, 'fr', 1, 2),
+(75, 'la putain de sa mère\r\n', 2, 1),
+(76, 'a', 2, 1),
+(77, 'z', 1, 2),
+(78, 'a', 2, 1),
+(79, 'VICTOIRE', 2, 1),
+(80, 'VICTOIRE', 2, 1),
+(81, 'VICTOIRE', 2, 1),
+(82, 'VICTOIRE', 2, 1),
+(83, 'VICTOIRE', 2, 1),
+(84, 'VICTOIRE', 2, 1),
+(85, 'VICTOIRE', 2, 1),
+(86, 'VICTOIRE', 2, 1),
+(87, 'VICTOIRE', 2, 1),
+(88, 'VICTOIRE', 2, 1),
+(89, 'VICTOIRE', 2, 1),
+(90, 'VICTOIRE', 2, 1),
+(91, 'VICTOIRE', 2, 1),
+(92, 'VICTOIRE', 2, 1),
+(93, 'VICTOIRE', 2, 1),
+(94, 'VICTOIRE', 2, 1),
+(95, 'VICTOIRE', 2, 1),
+(96, 'VICTOIRE', 2, 1),
+(97, 'nsm', 2, 1),
+(98, 'on game ce soir ', 2, 1),
+(99, 'ntm\r\n', 1, 2),
+(100, 'je te prends toi et ta maman sur le vieux port', 2, 1),
+(101, 'slt', 2, 1),
+(102, 'ta mère', 2, 1),
+(103, 'slt comment tu vas', 2, 1),
+(104, 'salut faisons connaissance', 2, 3),
+(105, 'ntm', 1, 2),
+(106, 'slt', 1, 2),
+(107, 'ntm', 2, 1),
+(108, 'lmp', 2, 1),
+(109, '55', 1, 2),
+(110, 'j', 1, 2),
+(111, 'jk', 1, 2),
+(112, 'perso ca va', 1, 3),
+(113, 'je mange', 1, 3),
+(114, ' ', 1, 2),
+(115, 'hyiizea', 2, 1),
+(116, 'salit', 2, 1),
+(117, 's', 2, 1),
+(118, 's', 2, 1),
+(119, 's', 2, 1),
+(120, 's', 1, 2),
+(121, 'comment tu vas', 1, 2),
+(122, 'ça va', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
 --
 
 CREATE TABLE `post` (
@@ -87,7 +186,7 @@ CREATE TABLE `post` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `post`
+-- Dumping data for table `post`
 --
 
 INSERT INTO `post` (`ID_Post`, `ID_Emetteur`, `Lien`, `Description`, `Date`, `Lieu`, `Heure`, `Privé`) VALUES
@@ -100,12 +199,13 @@ INSERT INTO `post` (`ID_Post`, `ID_Emetteur`, `Lien`, `Description`, `Date`, `Li
 (9, 3, 'documents/post/teemo.png', '', '', '', '', 1),
 (10, 1, 'documents/post/fond2.png', 'test', '', '', '', 0),
 (11, 5, 'documents/post/photo1 (2).jpg', '', '', '', '', 0),
-(12, 5, 'documents/post/did.mp4', '', '', '', '', 1);
+(12, 5, 'documents/post/did.mp4', '', '', '', '', 1),
+(14, 1, 'documents/post/5472d1b09d3d724228109d381d617326.jpg', '', '', '', '', 1);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `reaction`
+-- Table structure for table `reaction`
 --
 
 CREATE TABLE `reaction` (
@@ -113,55 +213,27 @@ CREATE TABLE `reaction` (
   `ID_Post` int(11) NOT NULL,
   `Description` varchar(200) DEFAULT NULL,
   `ID_Emetteur` int(11) NOT NULL,
-  `aime` tinyint(1) DEFAULT '0'
+  `aime` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `reaction`
+-- Dumping data for table `reaction`
 --
 
 INSERT INTO `reaction` (`ID`, `ID_Post`, `Description`, `ID_Emetteur`, `aime`) VALUES
-(2, 129, NULL, 4, 1),
-(3, 131, NULL, 1, 0),
-(4, 132, NULL, 1, 0),
-(5, 132, 'azerty\r\n', 4, 0),
-(6, 129, NULL, 10, 1),
-(7, 129, NULL, 11, 1),
-(8, 129, NULL, 13, 1),
-(42, 130, 'lil', 4, 0),
-(43, 131, NULL, 4, 0),
-(44, 131, 'test', 2, 0),
-(45, 129, NULL, 666, 55),
-(46, 135, NULL, 4, 1),
-(47, 138, 'test 1', 4, 0),
-(48, 130, 'test', 2, 0),
-(49, 135, NULL, 2, 0),
-(50, 129, 'Ceci est la vérité ', 2, 0),
-(53, 129, 'wallah c vrai', 1, 0),
-(54, 1, 'Merci à tous', 1, 1),
-(55, 1, 'Un beau couché de soleil !', 2, 1),
-(56, 1, 'Pas mal', 3, 1),
-(57, 5, NULL, 3, 1),
-(58, 9, NULL, 3, 1),
-(59, 3, NULL, 1, 0),
-(60, 8, NULL, 1, 0),
-(61, 3, NULL, 3, 0),
-(62, 8, 'Magnifique sa mère (de Kyliann) !', 2, 0),
-(63, 5, 'Une belle image de moi !', 2, 0),
-(64, 4, NULL, 2, 0),
-(65, 3, NULL, 2, 0),
-(66, 2, NULL, 2, 0),
-(67, 5, NULL, 1, 0),
-(68, 3, NULL, 5, 1),
-(69, 1, 'C\'est beau !', 5, 1),
-(70, 10, '\'a\'', 5, 0),
-(71, 5, '\"', 5, 0),
-(72, 10, 'cuzhczba', 1, 0);
+(1, 8, 'super', 1, 0),
+(2, 1, NULL, 1, 0),
+(3, 8, NULL, 2, 0),
+(4, 3, NULL, 1, 0),
+(5, 14, NULL, 1, 0),
+(6, 3, NULL, 2, 0),
+(7, 4, NULL, 2, 0),
+(8, 1, NULL, 2, 0);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `relation`
+-- Table structure for table `relation`
 --
 
 CREATE TABLE `relation` (
@@ -171,121 +243,131 @@ CREATE TABLE `relation` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `relation`
+-- Dumping data for table `relation`
 --
 
 INSERT INTO `relation` (`ID_relation`, `ID_demandeur_ami`, `ID_ami`) VALUES
-(1, 2, 1),
-(2, 1, 2),
-(3, 18, 1),
-(4, 18, 2);
+(13, 1, 2),
+(14, 1, 3);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `utilisateur`
+-- Table structure for table `utilisateur`
 --
 
 CREATE TABLE `utilisateur` (
   `ID` int(11) NOT NULL,
-  `Pseudo` varchar(255) NOT NULL,
-  `Email` varchar(255) NOT NULL,
-  `MDP` varchar(255) NOT NULL,
-  `NC` int(11) NOT NULL,
-  `Image` varchar(255) DEFAULT 'documents/site/fond.jpg',
-  `Photo` varchar(255) DEFAULT 'documents/site/photo.jpg',
+  `Pseudo` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `MDP` varchar(255) DEFAULT NULL,
+  `NC` int(11) DEFAULT NULL,
+  `Image` varchar(255) DEFAULT NULL,
+  `Photo` varchar(255) DEFAULT NULL,
   `Description` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `utilisateur`
+-- Dumping data for table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`ID`, `Pseudo`, `Email`, `MDP`, `NC`, `Image`, `Photo`, `Description`) VALUES
-(1, 'Sowgan', 's.g@gmail.com', '123', 1, 'documents/fond/test.jpg', 'documents/photo/photo.jpg', 'Je peux changer la description :)'),
-(2, 'Teemo', 'Teemo.demon@faille.invocateur', '456', 1, 'documents/fond/teemo.png', 'documents/photo/17018.jpg', 'Je suis un vrai démon qui flash Q pour voler des kills'),
-(3, 'test', 'test@gmail.com', '789', 0, 'documents/fond/baby-groot-4k-hd-superheroes-wallpaper-preview.jpg', 'documents/photo/photo1 (2).jpg', ''),
-(5, 'TestFinal', 'Testfinal@gmail.com', '123', 1, 'documents/site/fond.jpg', 'documents/site/photo.jpg', 'Je suis le test final');
+(1, 'Sowgan', 's.g@gmail.com', '123', 1, 'documents/fond/fond1.jpeg', 'documents/photo/photo1.jpg', ''),
+(2, 'Teemo', 'Teemo.demon@faille.invocateur', '456', 0, 'documents/fond/teemo.png', 'documents/photo/teemo.png', ''),
+(3, 'Zoro', 'chiot@edu.fr', '741', 0, NULL, NULL, NULL),
+(4, 'yas', '123@mp.fr', '789', 0, NULL, NULL, NULL);
 
 --
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `emploi`
+-- Indexes for table `emploi`
 --
 ALTER TABLE `emploi`
   ADD PRIMARY KEY (`ID_Emploi`);
 
 --
--- Index pour la table `event`
+-- Indexes for table `event`
 --
 ALTER TABLE `event`
   ADD PRIMARY KEY (`ID_Event`);
 
 --
--- Index pour la table `post`
+-- Indexes for table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id_message`);
+
+--
+-- Indexes for table `post`
 --
 ALTER TABLE `post`
   ADD PRIMARY KEY (`ID_Post`);
 
 --
--- Index pour la table `reaction`
+-- Indexes for table `reaction`
 --
 ALTER TABLE `reaction`
   ADD PRIMARY KEY (`ID`);
 
 --
--- Index pour la table `relation`
+-- Indexes for table `relation`
 --
 ALTER TABLE `relation`
   ADD PRIMARY KEY (`ID_relation`);
 
 --
--- Index pour la table `utilisateur`
+-- Indexes for table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `emploi`
+-- AUTO_INCREMENT for table `emploi`
 --
 ALTER TABLE `emploi`
-  MODIFY `ID_Emploi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID_Emploi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT pour la table `event`
+-- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `ID_Event` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID_Event` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT pour la table `post`
+-- AUTO_INCREMENT for table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=123;
+
+--
+-- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `ID_Post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `ID_Post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT pour la table `reaction`
+-- AUTO_INCREMENT for table `reaction`
 --
 ALTER TABLE `reaction`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT pour la table `relation`
+-- AUTO_INCREMENT for table `relation`
 --
 ALTER TABLE `relation`
-  MODIFY `ID_relation` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID_relation` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT pour la table `utilisateur`
+-- AUTO_INCREMENT for table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
